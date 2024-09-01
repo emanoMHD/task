3acbb017a5bd7010df3a256e7af242181f3acfeb
eman <eman98.eng@gmail.com>
2024-09-01 23:51:00 +0300

    statistics

--- src/app/core/api.ts
+++ src/app/core/api.ts
@@ -7,31 +7,13 @@ frontUrl:"http:/localhost:4200"
 
 
 export const apiUrl={
-    baseUrl:"http://cloud.amescom.com/api/"
+    baseUrl:""
 
 }
-export const city={
-    cityUrl:apiUrl.baseUrl+'api/app/city',
 
-}
 export const account={
     login:apiUrl.baseUrl+'api/app/account/login',
 
 }
-export const country={
-    countryUrl:apiUrl.baseUrl+'api/app/country',
 
-}
-export const Categories=
-{
-    categoryUrl:apiUrl.baseUrl+'category'
-}
-export const Currencies=
-{
-    currencyUrl:apiUrl.baseUrl+'currency'
-}
-export const Produts=
-{
-productUrl:apiUrl.baseUrl+'product'
-}
 
